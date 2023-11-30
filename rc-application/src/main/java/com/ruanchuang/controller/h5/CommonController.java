350b7b0b804e2fd95c0c4e5cd5839cbbb3262ce4
guopeixiong <259880048@qq.com>
2023-11-30 21:19:36 +0800

    fix: 接口注解錯誤

--- rc-application/src/main/java/com/ruanchuang/controller/h5/CommonController.java
+++ rc-application/src/main/java/com/ruanchuang/controller/h5/CommonController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class CommonController {
 
     @ApiOperation("获取常见问题接口")
     @RateLimiter(key = "getForm", count = 1000, message = "服务器限流, 请稍后再试")
-    @RequestMapping("/getCommonQaInfo")
+    @GetMapping("/getCommonQaInfo")
     public CommonResult getCommonQaInfo() {
         return CommonResult.ok(commonQaInfoService.getEnableCommonQaInfo());
     }
